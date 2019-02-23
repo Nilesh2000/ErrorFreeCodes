bffebe62840bc71a01e0261998685ce7021cff3d
Nilesh2000 <nileshlund@gmail.com>
2019-02-23 22:40:10 +0530

    Inserting a node at the end of a linked list.

---
+++ LinkedLists/insertAtEnd.cpp
@@ -0,0 +1,50 @@
+//A program to insert elements at the end of a linked list.
+# include <iostream>
+
+using namespace std;
+
+struct Node
+{
+  int Data;
+  Node *Next;
+};
+
+Node *Head = NULL; //List is initially empty.
+
+void insertAtEnd(int x)
+{
+    Node *Temp = new Node(); //Allocate memory to create a new node.
+    Temp -> Data = x; //Initialize the Data of the node to the parameter passed.
+      if(Head == NULL) //Check if the list is initially empty.
+        {
+            Head = Temp; //Set the address of the newly allocated node to Head.
+            return ;
+        }
+    //If the list is not empty,
+
+    Node *lastNode = Head; //Set a temporary variable of type Node to Head.
+
+    while(lastNode -> Next != NULL) //Traverse the linked list till the end of the list.
+      {
+          lastNode = lastNode -> Next; //Keep moving to the next node.
+      }
+  lastNode -> Next = Temp; //Set the address of the last node to the newly allocated node in memory.
+}
+
+void printList()
+{
+    Node *Temp = Head;
+      while(Temp != NULL)
+        {
+            cout << Temp -> Data << " ";
+            Temp = Temp ->Next;
+        }
+}
+
+int  main(void)
+{
+    insertAtEnd(3);
+    insertAtEnd(5);
+    insertAtEnd(7);
+    printList();
+}
