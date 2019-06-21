16d3d89be38fa84f6648d4d3d5270f81126207de
Nilesh2000 <nileshlund@gmail.com>
2019-06-21 19:18:48 +0530

    The condition c1 < c2 is not working.

--- LinkedLists/intersectionOfTwoLists.cpp
+++ LinkedLists/intersectionOfTwoLists.cpp
@@ -11,18 +11,18 @@ struct Node
 
 int getIntersectionNode(Node *, Node *, int);
 
-void insertAtEnd(Node *A, int x) //A refers to the address of the head node.
+void insertAtEnd(Node **A, int x) //A refers to the address of the head node.
 {
     Node *Temp = new Node(); //Allocate memory to create a new node.
     Temp -> Data = x; //Initialize the Data of the node to the parameter passed.
-      if(A == NULL) //Check if the list is initially empty.
+      if((*A) == NULL) //Check if the list is initially empty.
         {
-            A = Temp; //Set the address of the newly allocated node to Head.
+            (*A) = Temp; //Set the address of the newly allocated node to Head.
             return ;
         }
     //If the list is not empty,
 
-    Node *lastNode = A; //Set a temporary variable of type Node to Head.
+    Node *lastNode = *A; //Set a temporary variable of type Node to Head.
 
     while(lastNode -> Next != NULL) //Traverse the linked list till the end of the list.
       {
@@ -81,49 +81,47 @@ int getIntersectionNode(Node *A1, Node *A2, int d) //A1 and A2 are addresses of 
 
 int main(void)
 {
-    Node *Head1 = new Node();
-    Node *Head2 = new Node();
-
-    /*
-
-    T1 (c1 > c2)
+    Node *Head1 = NULL;
+    Node *Head2 = NULL;
+/*
+    //T1 (c1 > c2)
     //List 1 : 3 6 9 15 30
     //List 2 : 10 15 25
-    insertAtEnd(Head1, 3);
-    insertAtEnd(Head1, 6);
-    insertAtEnd(Head1, 9);
-    insertAtEnd(Head1, 15);
-    insertAtEnd(Head1, 30);
-    insertAtEnd(Head2, 10);
-    insertAtEnd(Head2, 15);
-    Head1 -> Next -> Next -> Next -> Next = Head2 -> Next -> Next; //The address for the data 15 should be same in both the lists.
-    insertAtEnd(Head2, 25);
+    insertAtEnd(&Head1, 3);
+    insertAtEnd(&Head1, 6);
+    insertAtEnd(&Head1, 9);
+    insertAtEnd(&Head1, 15);
+    insertAtEnd(&Head1, 30);
+    insertAtEnd(&Head2, 10);
+    insertAtEnd(&Head2, 15);
+    Head1 -> Next -> Next -> Next = Head2 -> Next; //The address for the data 15 should be same in both the lists.
+    insertAtEnd(&Head2, 25);
 
     //T2 (c1 == c2)
     //List 1 : 3 6 15
     //List 2 : 10 15 25
-    insertAtEnd(Head1, 3);
-    insertAtEnd(Head1, 6);
-    insertAtEnd(Head1, 15);
-    insertAtEnd(Head2, 10);
-    insertAtEnd(Head2, 15);
-    Head1 -> Next -> Next -> Next = Head2 -> Next -> Next; //The address for the data 15 should be same in both the lists.
-    insertAtEnd(Head2, 25);
-
+    insertAtEnd(&Head1, 3);
+    insertAtEnd(&Head1, 6);
+    insertAtEnd(&Head1, 15);
+    insertAtEnd(&Head2, 10);
+    insertAtEnd(&Head2, 15);
+    Head1 -> Next -> Next = Head2 -> Next; //The address for the data 15 should be same in both the lists.
+    insertAtEnd(&Head2, 25);
+*/
     //T3 (c1 < c2)
     //List 1 : 3 15
-    //List 2 : 10 15 25 35 45 55 65
-    insertAtEnd(Head1, 3);
-    insertAtEnd(Head1, 15);
-    insertAtEnd(Head2, 10);
-    insertAtEnd(Head2, 15);
-    Head1 -> Next -> Next = Head2 -> Next -> Next; //The address for the data 15 should be same in both the lists.
-    insertAtEnd(Head2, 25);
-    insertAtEnd(Head2, 35);
-    insertAtEnd(Head2, 45);
-    insertAtEnd(Head2, 55);
-    insertAtEnd(Head2, 65);*/
-    
+    //List 2 : 10 12 15 35 45 55 65
+    insertAtEnd(&Head1, 3);
+    insertAtEnd(&Head1, 15);
+    insertAtEnd(&Head2, 10);
+    insertAtEnd(&Head2, 12);
+    insertAtEnd(&Head2, 15);
+    Head1 -> Next = Head2 -> Next -> Next; //The address for the data 15 should be same in both the lists.
+    insertAtEnd(&Head2, 35);
+    insertAtEnd(&Head2, 45);
+    insertAtEnd(&Head2, 55);
+    insertAtEnd(&Head2, 65);
+
     cout << "The Data Stored In The Node At The Point Of Intersection Is : " << pointOfIntersection(Head1, Head2) << endl;
     return 0;
 }
