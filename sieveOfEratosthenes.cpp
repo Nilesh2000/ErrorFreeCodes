f939ffb6fae502691be97c706ee317abf5535abf
Nilesh2000 <nileshlund@gmail.com>
2019-06-25 18:39:55 +0530

    Added comments.

--- sieveOfEratosthenes.cpp
+++ sieveOfEratosthenes.cpp
@@ -6,17 +6,20 @@ using namespace std;
 int sieveOfEratosthenes(int n)
 {
     bool Prime[n + 1]; //Define a boolean variable Prime of size n+1. Size is n+1 because it consists of elements from 0 to n.
-    memset(Prime, true, sizeof(Prime));
+    memset(Prime, true, sizeof(Prime)); //Iniialize all entries to true in the boolean array.
     for(int i = 2 ; i * i <= n ; i++)
     {
-        if(Prime[i] == true)
+        if(Prime[i] == true) //If the number is prime, set all it's multiples to not prime.
         {
+          //Update all multiples of p greater than or  equal to the square of it numbers which are multiple of p and are less than p^2 are already
+          //been marked.
             for(int j = i * i ; j <= n ; j+=i)
             {
                 Prime[j] = false;
             }
         }
     }
+    //Print all the prime numbers
     for(int i = 2 ; i <= n ; i++)
     {
         if(Prime[i])
@@ -26,7 +29,7 @@ int sieveOfEratosthenes(int n)
     }
 }
 
-int main(void)
+int main(void) //Driver function
 {
     int n;
     cout << "Enter n : ";
