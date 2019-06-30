ca2ca79e29c4a2f42c91ece9f6ab779c7349bddc
Nilesh2000 <nileshlund@gmail.com>
2019-06-30 13:05:41 +0530

    Solved the tower of hanoi problem.

---
+++ towerOfHanoi.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+
+using namespace std;
+
+void towerOfHanoi(int n, char fromRod, char toRod, char auxRod)
+{
+    if(n == 1)
+    {
+        cout << "\nMove disk 1 from rod " << fromRod << " to " << toRod;
+        return ;
+    }
+    towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+    cout << "\nMove disk " << n << " from rod " << fromRod << " to rod " << toRod;
+    towerOfHanoi(n - 1, auxRod, toRod, fromRod);
+}
+
+int main()
+{
+    int n;
+    cout << "\nEnter the number of disks : ";
+    cin >> n;
+    towerOfHanoi(n, 'A', 'C', 'B');
+    return 0;
+}
