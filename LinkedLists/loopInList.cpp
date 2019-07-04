43980f9e9905fbb321569cb758747b03b353402e
Nilesh D <nileshlund@gmail.com>
2019-07-04 19:38:45 +0530

    Added explaination and function to calculate the first node of the cycle

--- LinkedLists/loopInList.cpp
+++ LinkedLists/loopInList.cpp
@@ -1,4 +1,29 @@
 //An implementation of the Floyd's-Cycle finding algorithm to detect if a loop exists and to find the length of the loop in a linked list.
+
+/*
+Explaination :-
+Let the length of the cycle be n.
+Let m be the number of nodes to the cycle initially.
+Let the hare and tortoise pointer meet a k steps from the start of the cycle.
+Let the tortoie go through the cycle p times and the hare go through the cycle q times.
+Then,
+i = m + (p*n) + k
+2i = m + (q*n) + k
+We are using 2i for the hare since it moves two nodes at a time whereas the tortoie moves only one node at a time.
+Thus,
+2{m + (p*n) + k} = m + (q*n) + k
+m + k = (q - 2p)n
+
+Finding the first node of the cycle:-
+Once the hare and tortoise meet, put the tortoise pointer back to head and leave hare wherever they met which k steps from the start of the cycle.
+Then, of we let them move k steps the tortoise would be back at the place where they met initially.
+Since m + k steps is a multiple of cycle length n, hare, in the mean time, would go through the cycle (q-2p) times and would come back to the same point
+(k steps away from the cycle beginning).
+Now, instead of letting them move m + k steps, if we let them move only m steps, tortoise would arrive at the cycle beginning. Hare would go be k steps short
+of completing (q-2p) rotations. Since it started k steps in front of the cycle beginning, hare would have to arrive at the cycle beginning.
+As a result, this explains that they would have to meet at the cycle beginning after some number of steps for the very first time (very first time because
+tortoise just arrived at the cycle after m steps and it could never see hare which was already in the cycle).
+*/
 # include <iostream>
 
 using namespace std;
@@ -12,6 +37,7 @@ struct Node
 Node *Head = NULL; //List is initially empty.
 
 int lengthOfLoop(Node*);
+int firstNodeOfLoop(Node*, Node*);
 
 void insertAtEnd(int x)
 {
@@ -34,8 +60,8 @@ void insertAtEnd(int x)
 }
 
 /*
-Traverse linked list using two pointers.  Move one pointer by one and other pointer by two.  If these pointers meet at same node then there is a
-loop.  If pointers do not meet then linked list doesn’t have loop.
+Traverse linked list using two pointers.  Move one pointer by one noce and other pointer by two nodes.  If these pointers meet at same node,  then there is a
+loop.  If pointers do not meet then linked list doesn’t have a loop.
 */
 void detectLoop(Node *A)
 {
@@ -47,8 +73,9 @@ void detectLoop(Node *A)
             Fast_Ptr = Fast_Ptr -> Next -> Next;
               if(Slow_Ptr == Fast_Ptr)
                 {
-                    cout << "\nLoop Exists In The Linked List." << endl;
-                    cout << "The Length Of the loop is : " << lengthOfLoop(Slow_Ptr) << endl;
+                    cout << "\nLoop Exists In The Linked List.";
+                    cout << "\nThe Length Of the loop is : " << lengthOfLoop(Slow_Ptr);
+                    cout << "\nThe first node of the cycle is : " << firstNodeOfLoop(Slow_Ptr, Fast_Ptr) << endl;
                     return ;
                 }
         }
@@ -71,6 +98,18 @@ int lengthOfLoop(Node *n) //n refers to one of the nodes present in the loop of 
         }
     return Len;
 }
+
+int firstNodeOfLoop(Node *slowPtr, Node *fastPtr)
+{
+    slowPtr = Head;
+    while(slowPtr != fastPtr)
+    {
+        slowPtr = slowPtr -> Next;
+        fastPtr = fastPtr -> Next;
+    }
+    return slowPtr -> Data;
+}
+
 int main(void)
 {
     insertAtEnd(3);
@@ -78,7 +117,7 @@ int main(void)
     insertAtEnd(7);
     insertAtEnd(9);
     insertAtEnd(11);
-    Head -> Next -> Next -> Next = Head; //Test case to check if the loop exists in the linked list.
+    Head -> Next -> Next -> Next -> Next = Head -> Next; //Test case to check if the loop exists in the linked list.
     detectLoop(Head);
     return 0;
 }
