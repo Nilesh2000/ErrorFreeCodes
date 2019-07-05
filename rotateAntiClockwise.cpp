3aaa6b6e6f01538aba7b122e5e3c97115a346900
Nilesh D <nileshlund@gmail.com>
2019-07-05 08:35:02 +0530

    Program to rotate a matrix in the anti-clockise dierection.

---
+++ rotateAntiClockwise.cpp
@@ -0,0 +1,58 @@
+//A program to rotate a 2-dimensional matrix in the anti-clockwise dierection.
+# include <iostream>
+
+using namespace std;
+
+//Function to print the matrix
+void printMatrix(int Arr[][3])
+{
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        for(int j = 0 ; j < 3 ; j++)
+        {
+            cout << Arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+//The idea here is to first transpose the existing matrix and then reverse the columns of the result transpose matrix. In this way, we get a matrix which has
+//been rotated in the anti-clockwise dierection by 90 degrees.
+void rotateAntiClockwise(int Arr[][3])
+{
+    int Transpose[3][3];
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        for(int j = 0 ; j < 3 ; j++)
+        {
+            Transpose[i][j] = Arr[j][i];
+        }
+    }
+
+    int n = 3;
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0, k = n - 1 ; j < n / 2 ; j++, k--)
+        {
+            int Temp = Transpose[j][i];
+            Transpose[j][i] = Transpose[k][i];
+            Transpose[k][i] = Temp;
+        }
+    }
+    printMatrix(Transpose);
+}
+
+//Driver function
+int main(void)
+{
+    int Mat[3][3] = {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+                    };
+    printMatrix(Mat);
+    cout << "\nMatrix after rotating in anti-clockwise dierection:-\n";
+    rotateAntiClockwise(Mat);
+    return 0;
+}
+//End of program
