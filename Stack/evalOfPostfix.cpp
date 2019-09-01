f27dedf4799d14f80ab9458aad029b7df9e2ea22
Nilesh D <nileshlund@gmail.com>
2019-09-01 09:03:41 +0530

    Added feature for operations on multiple digits.

--- Stack/evalOfPostfix.cpp
+++ Stack/evalOfPostfix.cpp
@@ -8,17 +8,30 @@ using namespace std;
 int evalExp(string Exp)
 {
     stack <int> S; //Decalre an integer stack to store the operands.
-    for(int i = 0 ; Exp[i] != '\0' ; i++) //Parse the passes Postfix expression.
+    for(int i = 0 ; Exp[i] != '\0' ; i++) //Parse the passed Postfix expression.
     {
-        if(isdigit(Exp[i])) //If the read character is a digit / operand
+        if(Exp[i] == ' ')
         {
-            S.push(Exp[i] - '0'); //Push it into the stack.
+            continue;
         }
+
+        else if(isdigit(Exp[i])) //If the read character is a digit / operand
+        {
+            int Num = 0;
+            while(isdigit(Exp[i]))
+            {
+                Num = Num * 10 + (int)(Exp[i] - '0');
+                i++;
+            }
+            i--;
+            S.push(Num); //Push it into the stack.
+        }
+
         else //When you encounter an operator, pop the first two values and perform an operation.
         {
-            int Val1 = S.top();
-            S.pop();
             int Val2 = S.top();
+            S.pop();
+            int Val1 = S.top();
             S.pop();
             switch(Exp[i]) //After performing the resultant operation, push the result into the stack.
             {
@@ -32,7 +45,6 @@ int evalExp(string Exp)
                             break;
             }
         }
-
     }
     return S.top(); //Return the topmost element of the stack to main() which also happens to be the only element present in the stack.
 }
@@ -41,7 +53,7 @@ int main(void)
 {
     string postFix;
     cout << "\nEnter the postfix expression : ";
-    cin >> postFix;
+    getline(cin, postFix);
     int Res = evalExp(postFix);
     cout << "Evluation Of the expression results in : " << Res;
     return 0;
