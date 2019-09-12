e131ea8c36776cc008494ba4230dc85101c3b77d
Nilesh D <nileshlund@gmail.com>
2019-09-12 20:10:22 +0530

    program to remove duplicate characters from a string and output it in sorted order.

---
+++ removeDuplicatesInSorted.cpp
@@ -0,0 +1,18 @@
+//A program to remove duplicate characters from a string and output it in sorted order.
+# include <iostream>
+# include <set>
+# include <string>
+
+using namespace std;
+
+int main(void)
+{
+    string str = "GeeksForGeeks";
+    set <char> S;
+    for(int i = 0 ; i < str.length() ; i++)
+    {
+        S.insert(str[i]);
+    }
+    for(auto it : S)
+      cout << it;
+}
