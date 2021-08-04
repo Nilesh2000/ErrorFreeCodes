a0e58b0afc0c68f4cc1d016675105e217365a9b5
Nilesh D <nileshlund@gmail.com>
2021-08-04 10:58:29 +0530

    fix: Move file

--- BinarySearch/peakElement.cpp
+++ BinarySearch/peakElement.cpp
@@ -12,30 +12,38 @@ https://leetcode.com/problems/find-peak-element/discuss/50232/Find-the-maximum-b
 
 #include <iostream>
 using namespace std;
-int findPeakIndex(int Arr[], int n, int Low, int High)
-{
-  int Mid = Low + (High - Low) / 2;
 
-  if ((Mid == 0 || Arr[Mid] >= Arr[Mid - 1]) && (Mid == n - 1 || Arr[Mid] <= Arr[Mid + 1]))
-    return Mid;
-  else if (Arr[Mid] < Arr[Mid - 1] && Mid > 0)
-    findPeakIndex(Arr, n, 0, Mid - 1);
-  else
-    findPeakIndex(Arr, n, Mid + 1, High);
+int findPeakIndex(int Arr[], int n)
+{
+  if (n == 1)
+    return 0;
+
+  if (Arr[0] > Arr[1])
+    return 0;
+  if (Arr[n - 1] > Arr[n - 2])
+    return n - 1;
+
+  int Low = 1, High = n - 2;
+
+  while (Low <= High)
+  {
+    int mid = Low + (High - Low) / 2;
+    if (Arr[mid] > Arr[mid - 1] && Arr[mid] > Arr[mid + 1])
+      return mid;
+    else if (Arr[mid] < Arr[mid - 1])
+      High = mid - 1;
+    else if (Arr[mid] < Arr[mid + 1])
+      Low = mid + 1;
+  }
+
+  return -1;
 }
 
 int main(void)
 {
-  int n;
-  cout << "\nEnter the size of the array : ";
-  cin >> n;
-  int Arr[n];
-  cout << "Enter the elements of the array : ";
-  for (int i = 0; i < n; i++)
-  {
-    cin >> Arr[i];
-  }
-  int Index = findPeakElement(Arr, n, 0, n - 1);
-  cout << "\nIndex of peak element is : " << Index << " and the element is : " << Arr[Index];
+  int Arr[] = {1, 2, 3, 2};
+  int n = sizeof(Arr) / sizeof(Arr[0]);
+  int Index = findPeakIndex(Arr, n);
+  cout << "\nIndex of peak element is " << Index << " and the element is " << Arr[Index];
   return 0;
 }
