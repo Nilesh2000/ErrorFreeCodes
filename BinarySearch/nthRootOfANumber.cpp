13512d8793c2214229e45cbc87f989f6a35952a6
Nilesh D <nileshlund@gmail.com>
2021-08-14 08:20:47 +0530

    feat: Program to calculate the Nth root of a number using Binary Search

---
+++ BinarySearch/nthRootOfANumber.cpp
@@ -0,0 +1,45 @@
+/*
+Given an integer m, calculate it's nth root
+
+Time Complexity : O(n * log(m * 10^d))
+Space Complexity : O(1)
+*/
+#include <iostream>
+
+using namespace std;
+
+double multiply(double Num, int n)
+{
+    double Ans = 1.0;
+    for (int i = 1; i <= n; i++)
+        Ans *= Num;
+    return Ans;
+}
+
+double findNthRoot(int m, int n)
+{
+    double Low = 1.0;
+    double High = m, Mid;
+    // eps represents the precision we want in our answer
+    // 1e-6 means we want the precision to be 5 decimal places
+    double eps = 1e-6;
+
+    while (High - Low > eps)
+    {
+        Mid = (Low + High) / 2.0;
+        if (multiply(Mid, n) < m)
+            Low = Mid;
+        else
+            High = Mid;
+    }
+
+    return Mid;
+}
+
+int main(void)
+{
+    int m, n;
+    m = 16, n = 2;
+    cout << findNthRoot(m, n);
+    return 0;
+}
