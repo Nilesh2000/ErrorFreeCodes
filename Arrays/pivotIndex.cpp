ec63df7bf6d5a0f6023e1988592f450251db30e4
Nilesh D <nileshlund@gmail.com>
2021-08-17 12:54:37 +0530

    feat: Program to find the pivot index of an array

--- Arrays/pivotIndex.cpp
+++ Arrays/pivotIndex.cpp
@@ -1,3 +1,15 @@
+/*
+Given an array of integers nums, calculate the pivot index of this array.
+
+Calculate the sum of the array
+Set leftSum=0
+Traverse array again and on each iteration check if leftSum==arraySum before performing arraySum-=Arr[i].
+Increment leftSum by Arr[i] after each if statement
+
+Time Complexity : O(n)
+Space Complexity : O(1)
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -20,15 +32,8 @@ int equiliburumIndex(int Arr[], int n)
 
 int main(void)
 {
-  int n;
-  cout << "\nEnter the size of the array : ";
-  cin >> n;
-  int Arr[n];
-  cout << "Enter the elements of the array : ";
-  for (int i = 0; i < n; i++)
-  {
-    cin >> Arr[i];
-  }
+  int Arr[] = {1, 7, 3, 6, 5, 6};
+  int n = sizeof(Arr) / sizeof(Arr[0]);
 
   int Index = equiliburumIndex(Arr, n);
   if (Index != -1)
