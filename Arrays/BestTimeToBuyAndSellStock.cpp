cd54215224398d1eaecb9df5ffc08ee131584c01
Nilesh D <nileshlund@gmail.com>
2021-09-21 22:15:56 +0530

    feat: Program to maximize profit on buying and selling a stock once

---
+++ Arrays/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,53 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. 
+If you cannot achieve any profit, return 0.
+
+APPROACH 1:
+Iterate i from 0 to length of vector
+    Iterate j from i+1 to length of vector
+        if V[j]-V[i] > maxProfit
+            maxProfit = V[j] - V[i]
+
+Time Complexity : O(N^2)
+Space Complexity : O(1)
+
+APPROACH 2:
+Iterate i from 0 to length of vector.
+For every element:
+    Calculate the difference between that element and the minimum of all the values before that element and we are updating the maximum profit if the difference thus found is greater than the current maximum profit.
+    If that array element is smaller than all array elements before it, we update it to be the smallest element.
+
+Time Complexity : O(N)
+Space Complexity : O(1)
+*/
+
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int maxProfit(vector<int> V)
+{
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (auto num : V)
+    {
+        if (num < minPrice)
+            minPrice = num;
+        else if (num - minPrice > maxProfit)
+            maxProfit = num - minPrice;
+    }
+    return maxProfit;
+}
+
+int main(void)
+{
+    vector<int> V = {7, 1, 5, 3, 6, 4};
+    // vector<int> V = {4, 4, 4, 4};
+    cout << maxProfit(V);
+    return 0;
+}
