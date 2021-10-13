89a87f3a3fbd3fe3b2cb6cc829a50a7a9290bd41
Nilesh D <nileshlund@gmail.com>
2021-10-13 12:07:12 +0530

    feat: Program to illustrate min-heap

--- Heaps/minHeap.cpp
+++ Heaps/minHeap.cpp
@@ -1,207 +1,168 @@
-// A C++ program to demonstrate common MinHeap Operations
-# include <iostream>
-# include <climits> //Used for INT_MAX and INT_MIN
+/*
+Illustration of a min-heap.
+
+Heaps come in two forms, namely min-heap and max-heap.
+In a min-heap, all the elements are actually smaller than their children.
+Thus, root node will be the smallest element in the heap.
+Looking down the heap, the elements get bigger.
+
+A heap is represented as a binary tree for easy understanding.
+However, it is implemented as an array.
+
+Array Implementation:
+1. If the index of an element is i, then index of parent element (i-1)/2
+2. Indices of the two children will be (2*i)+1 and (2*i)+2.
+
+Insertion into a heap:-
+1. Insert at the last empty position in the array.
+2. However, it is not necessary that the element may be inserted at the right position.
+3. Hence, after insertion, we bubble it up to its correct place.
+
+Deletion the smallest element:-
+1. The root node will be the smallest element
+2. Thus, we first store value present in V[0].
+3. Then, we set V[0] to V[Size-1].
+4. This may not be the right position for V[0].
+5. Hence, we bubble down the heap.
+*/
+
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cstring>
+
 using namespace std;
 
-//Function to swap two integers
-void swap(int *x, int *y)
+class minHeap
 {
-  int Temp = *x;
-  *x = *y;
-  *y = Temp;
-}
+private:
+    int size;
+    int capacity;
+    int *heapArr;
 
-// A class for Min Heap
-struct MinHeap
-{
-	int *heapArray; // pointer to array of elements in heap
-	int Capacity; // maximum possible size of min heap
-	int heapSize; // Current number of elements in min heap
+    // Helper methods
+    int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
+    int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
+    int getParentIndex(int childIndex) { return (childIndex - 1) / 2; }
 
-  //Constructor
-	MinHeap(int maxCap)
-  {
-    heapSize = 0;
-    Capacity = maxCap;
-    heapArray = new int[maxCap];
-  }
+    bool hasLeftChild(int index) { return getLeftChildIndex(index) < size; }
+    bool hasRightChild(int index) { return getRightChildIndex(index) < size; }
+    bool hasParent(int index) { return getParentIndex(index) >= 0; }
 
-	//To heapify a subtree with the root at given index
-	void MinHeapify(int);
+    int leftChild(int index) { return heapArr[getLeftChildIndex(index)]; }
+    int rightChild(int index) { return heapArr[getRightChildIndex(index)]; }
+    int parent(int index) { return heapArr[getParentIndex(index)]; }
 
-	int Parent(int i)
-  {
-     return (i - 1)/2;
-  }
-
-	//to get index of left child of node at index i
-	int leftChild(int i)
-  {
-    return ((2 * i) + 1);
-  }
-
-	//to get index of right child of node at index i
-	int rightChild(int i)
-  {
-    return ((2 * i) + 2);
-  }
-
-	//To extract/Delete the root which is the minimum element
-	int extractMin();
-
-	//Decreases key value of key at index i to newVal
-	void decreaseKey(int i, int newVal);
-
-	//Returns the minimum key (key at root) from min heap
-	int getMin()
-  {
-    return heapArray[0]; //Root is always stored as the first element of the array.
-  }
-
-	// Deletes a key stored at index i
-	void deleteKey(int i);
-
-	// Inserts a new key 'k'
-	void insertKey(int k);
-};
-
-//Inserts a new key 'k'
-void MinHeap::insertKey(int k)
-{
-	if (heapSize == Capacity)
-	{
-		cout << "Overflow: Could not insert Key\n";
-		return;
-	}
-
-	//First insert the new key at the end
-	heapSize++;
-	int i = heapSize - 1;
-	heapArray[i] = k;
-
-	// Fix the min heap property if it is violated
-	while (i != 0 && heapArray[Parent(i)] > heapArray[i])
-	{
-    swap(&heapArray[i], &heapArray[Parent(i)]);
-    i = Parent(i);
-	}
-  cout << k << " was inserted into the heap.\n";
-}
-
-// Decreases value of key at index 'i' to newVal. It is assumed that newVal is smaller than heapArray[i].
-void MinHeap::decreaseKey(int i, int newVal)
-{
-	heapArray[i] = newVal;
-	while (i != 0 && heapArray[Parent(i)] > heapArray[i])
-	{
-    swap(&heapArray[i], &heapArray[Parent(i)]);
-    i = Parent(i);
-	}
-}
-
-//Method to remove minimum element (or root) from min heap
-int MinHeap::extractMin()
-{
-	if (heapSize <= 0)
-		return INT_MAX;
-
-	if (heapSize == 1)
-	{
-		heapSize--;
-		return heapArray[0];
-	}
-
-	// Store the minimum value, and remove it from heap
-	int Root = heapArray[0];
-	heapArray[0] = heapArray[heapSize - 1]; //We now initialize the last element of the array as the root element.
-	heapSize--; //Then, decrease the size of the heap. Now you know why we transferred the last element of the array to the root.
-	MinHeapify(0); //heapify the element at index 0 which is the root.
-	return Root;
-}
-
-// This function deletes key at index i. It first reduced value to minus infinity, then calls extractMin().
-void MinHeap::deleteKey(int i)
-{
-	decreaseKey(i, INT_MIN);
-	extractMin();
-  cout << i << " was deleted from the heap.\n";
-}
-
-/*
-A recursive method to heapify a subtree with the root at given index. This method assumes that the subtrees are already heapified
-The logic is as follows:-
-We first assume that the root has the minElement element.
-Then, we compare the left and right child of the root.
-If any one of them is smaller than the root, then we make that element as the root and call MinHeapify() recursively.
-Else, if the root is the smaller than the left and right child, we exit from the function.
-*/
-void MinHeap::MinHeapify(int i)
-{
-	int l = leftChild(i);
-	int r = rightChild(i);
-	int minElement = i;
-
-	if (l < heapSize && heapArray[l] < heapArray[i])
-		minElement = l;
-	if (r < heapSize && heapArray[r] < heapArray[minElement])
-		minElement = r;
-
-	if (minElement != i)
-	{
-		swap(&heapArray[i], &heapArray[minElement]);
-		MinHeapify(minElement);
-	}
-
-}
-
-void Menu()
-{
-    int Choice = 0;
-    MinHeap Obj(10);
-    while(Choice != 6)
+    void swap(int indexOne, int indexTwo)
     {
-        cout << "\nEnter Any One Of The Following Choices:-"
-             << "\n1. Insert Key Into The Heap"
-             << "\n2. Delete Key From The Heap"
-             << "\n3. Extract Minimum From The Heap"
-             << "\n4. Decrease Key Value"
-             << "\n5. Get Minimum Value Of The Heap"
-             << "\n6. Exit"
-             << "\nYour choice is : ";
-        cin >> Choice;
-        int x, y;
-        switch(Choice)
+        int t = heapArr[indexOne];
+        heapArr[indexOne] = heapArr[indexTwo];
+        heapArr[indexTwo] = t;
+    }
+
+    // If heap becomes full, resize the heap to store double the elements
+    void ensureExtraCapacity()
+    {
+        if (size == capacity)
         {
-            case 1 : cout << "\nEnter the key to be inserted : ";
-                     cin >> x;
-                     Obj.insertKey(x);
-                      break;
-
-            case 2 : cout << "\nEnter the key to be deleted : ";
-                     cin >> x;
-                     Obj.deleteKey(x);
-                      break;
-
-            case 3 : cout << '\n' << Obj.extractMin() << " has been deleted from the heap.\n";
-                      break;
-
-            case 4 : cout << "\nEnter the key to be decreased and the new value of the key : ";
-                     cin >> x >> y;
-                     Obj.decreaseKey(x, y);
-                      break;
-
-            case 5 : cout << "\nMinimum element in the heap is : " << Obj.getMin() << "\n";
-                      break;
-
-            case 6 : break;
-
-            default : cout << "\nInvalid Choice.\n";
+            int newSize = size * 2;
+            int *newArr = new int[newSize];
+            memcpy(newArr, heapArr, size * sizeof(int));
+            delete[] heapArr;
+            heapArr = newArr;
+            capacity *= 2;
         }
     }
-}
 
-int main()
+public:
+    // Initial Values
+    minHeap()
+    {
+        size = 0;
+        capacity = 5;
+        heapArr = new int[capacity];
+    }
+
+    // Extract the smallest element
+    int peek()
+    {
+        if (size == 0)
+            return INT_MIN;
+        return heapArr[0];
+    }
+
+    // Extract and delete the smallest element
+    int poll()
+    {
+        if (size == 0)
+            return INT_MIN;
+
+        int num = heapArr[0];
+        heapArr[0] = heapArr[size - 1];
+        size--;
+        heapifyDown();
+        return num;
+    }
+
+    // Insert into heap
+    void add(int num)
+    {
+        ensureExtraCapacity();
+        heapArr[size] = num;
+        size++;
+        heapifyUp();
+    }
+
+    void heapifyUp()
+    {
+        int index = size - 1; // Get index of last inserted element
+        // Bubble the element upwards
+        while (hasParent(index) && parent(index) > heapArr[index])
+        {
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    void heapifyDown()
+    {
+        // The index of the element to be bubbled down is 0
+        int index = 0;
+        // We check only for left child because if the node does not have left child, it defintely won't have right child
+        while (hasLeftChild(index))
+        {
+            // Get the smaller of the left and right children incase the right child exists
+            int smallerChildIndex = getLeftChildIndex(index);
+            if (hasRightChild(index) && rightChild(index) < leftChild(index))
+                smallerChildIndex = getRightChildIndex(index);
+
+            // If the heap condition is satisfied, exit
+            if (heapArr[index] < heapArr[smallerChildIndex])
+                break;
+            // Else, bubble down the heap
+            else
+                swap(index, smallerChildIndex);
+
+            index = smallerChildIndex;
+        }
+    }
+};
+
+int main(void)
 {
-  Menu();
-	return 0;
+    minHeap Obj;
+
+    Obj.add(10);
+    Obj.add(15);
+    Obj.add(20);
+    cout << Obj.peek() << '\n';
+
+    Obj.add(12);
+    Obj.add(19);
+    Obj.add(8);
+
+    cout << Obj.poll() << "\n";
+    cout << Obj.peek();
+
+    return 0;
 }
